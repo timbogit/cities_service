0537fadd2f03548672a28dcf89c559ce8a816cb1
Tim Schmelmer <tim.schmelmer@livingsocial.com>
2014-04-15 22:55:50 -0600

    Adding first stab at swagger docs

--- app/models/city.rb
+++ app/models/city.rb
@@ -3,6 +3,12 @@ class City < ActiveRecord::Base
 
   geocoded_by :geocode_string
 
+  validates_presence_of :name
+  validates_presence_of :state
+  validates_presence_of :country
+  validates_numericality_of :latitude, allow_nil: true
+  validates_numericality_of :longitude, allow_nil: true
+
   def geocode_string
     "#{name}_#{state}_#{country}"
   end
